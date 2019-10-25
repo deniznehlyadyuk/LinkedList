05d9c6d5a5d3939a89ce383d7ca8160f03816f92
Deniz Satır <45640934+deniznehlyadyuk@users.noreply.github.com>
2019-10-25 12:22:27 +0300

    node struct'ı linked list struct'ının içine taşındı.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-struct node{
-	int data;
-	node *next;
-	node *prev;
-};
-
 struct linkedList{
+	struct node{
+		int data;
+		node *next;
+		node *prev;
+	};
+	
 	node *head = NULL;
-	int length = 0;
 	node *tmp = NULL;
+	int length = 0;
 	
 	void *append(int data){
 		if (head == NULL){
@@ -32,6 +32,7 @@ struct linkedList{
 	
 	void goToIndex(int index){
 		tmp = head;
+		
 		if (head == NULL){
 			cout << "TypeError: 'NULL' object is not subscriptable"	<< endl;
 			exit(0);
@@ -41,7 +42,6 @@ struct linkedList{
 				cout << "IndexError: list index out of range" << endl;
 				exit(0);
 			}
-			
 			for (int i = 0; i < index; i++)
 				tmp = tmp->next;
 			return;
@@ -59,6 +59,7 @@ struct linkedList{
 	
 	int findValueIndex(int value){
 		tmp = head;
+		
 		if (head == NULL){
 			cout << "TypeError: 'NULL' object is not subscriptable"	<< endl;
 			exit(0);
@@ -85,6 +86,7 @@ struct linkedList{
 		goToIndex(index);
 		tmp->prev->next = tmp->next;
 		tmp->next->prev = tmp->prev;
+		
 		if (index == 0){
 			head = tmp->next;
 		}
@@ -95,6 +97,7 @@ struct linkedList{
 		findValueIndex(value);
 		tmp->prev->next = tmp->next;
 		tmp->next->prev = tmp->prev;
+		
 		if (tmp == head){
 			head = tmp->next;
 		}
@@ -110,27 +113,23 @@ struct linkedList{
 	
 	linkedList *copy(){
 		linkedList &newList = *(new linkedList);
-		
 		tmp = head;
 		
 		for(int i = 0; i < length; i++){
 			newList.append(tmp->data);
 			tmp = tmp->next;
 		}
-		
 		return &newList;
 	}
 	
 	void reverse(){
 		linkedList &newList = *(new linkedList);
-		
 		tmp = head;
 		
 		for(int i = -1; i >= length * -1; i--){
 			newList.append(tmp->prev->data);
 			tmp = tmp->prev;
 		}
-		
 		head->next = NULL;
 		head->prev = NULL;
 		delete head;
@@ -174,6 +173,7 @@ struct linkedList{
 	
 	void extend(linkedList *arr){
 		tmp = arr->head;
+		
 		for (int i = 0; i < arr->length; i++){
 			this->append(tmp->data);
 			tmp = tmp->next;
@@ -183,27 +183,22 @@ struct linkedList{
 	void sort(){
 		linkedList &arr = *(new linkedList);
 		tmp = this->head;
+		
 		for (int i = 0; i < this->length; i++){
 			arr.append(tmp->data);
 			tmp = tmp->next;
 		}
-		
 		int i, key, j;  
-	    for (i = 1; i < arr.length; i++) 
-	    {  
-	        key = arr[i];  
-	        j = i - 1;  
-	  
-	        /* Move elements of arr[0..i-1], that are  
-	        greater than key, to one position ahead  
-	        of their current position */
-	        while (j >= 0 && arr[j] > key) 
-	        {  
-	            arr[j + 1] = arr[j];  
-	            j = j - 1;  
-	        }  
-	        arr[j + 1] = key;  
-	    } 
+		
+	    	for (i = 1; i < arr.length; i++){ 
+	       		key = arr[i];  
+	        	j = i - 1;  
+	        	while (j >= 0 && arr[j] > key){
+	            		arr[j + 1] = arr[j];  
+	            		j = j - 1;  
+	        	}  
+	        	arr[j + 1] = key;  
+	    	} 
 		this->head->next = NULL;
 		this->head->prev = NULL;
 		this->head = arr.head;
